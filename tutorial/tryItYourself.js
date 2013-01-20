d4dd7745032871322b7e9279e7617cab1956e362
Sandy Phan <sandyphan@google.com>
2013-01-21 09:03:08 +1100

    commented out the editors, just focus on the contents

--- tutorial/tryItYourself.js
+++ tutorial/tryItYourself.js
@@ -28,7 +28,7 @@ function update() {
   var jsVal = document.getElementById('jsCode').value;
 
   // change the body and css in value in inframe
-  frames['display'].document.documentElement.innerHTML += htmlVal;
+  frames['display'].document.documentElement.innerHTML = htmlVal;
   
   var par;
 
@@ -43,8 +43,10 @@ function update() {
     par.appendChild(scriptEle);
   }
 
+  console.log(frames['display'].document.getElementsByTagName('head')[0]);
+
   par = frames['display'].document.getElementsByTagName('style')[0];
-   par.innerHTML = defaultCSSVal + cssVal;       
+  par.innerHTML = cssVal;       
 }
 
 var appendObjects = function() {
@@ -66,4 +68,69 @@ var toggleSolution = function() {
     ele.style.display = 'none';
     label.innerHTML = 'Show Solution';
   }
+}
+
+var generateEditor = function() {
+  // a wrapper for the whole try it editor section
+  var tryIt = document.querySelector('#tryIt');
+  console.log(tryIt);
+  console.log("hey");
+
+  // create a header banner for the editor
+  // button included in the section
+  var heading = document.createElement('div');
+  heading.className = 'heading';
+  heading.innerHTML = 'TRY IT YOURSELF';
+
+  var updateBtn = document.createElement('button');
+  updateBtn.setAttribute('onclick', 'update()');
+  updateBtn.setAttribute('id', 'update');
+  updateBtn.setAttribute('value', 'Update');
+  heading.appendChild(updateBtn);
+
+  tryIt.appendChild(heading);
+
+  // create text box elements and assign
+  // default values in it
+  var code = document.createElement('div');
+  code.className = 'code';
+
+  var htmlLabel = document.createElement('div');
+  htmlLabel.id = 'label'
+  htmlLabel.innerHTML = 'HTML Code';
+  code.appendChild(htmlLabel);
+
+  var htmlCode = document.createElement('textarea');
+  htmlCode.className = 'code';
+  htmlCode.id ='htmlCode';
+  htmlCode.innerHTML = '<html>' + '\n' + '  <head>' + '\n' + '    <style>' + '\n' + '    </style>' + '\n' + '  </head>' + '\n' + '  <body>' + '\n' + '    <div id="a" class="anim"></div>' + '\n' + '  </body>' + '\n' + '</html>';
+  code.appendChild(htmlCode);
+
+  var cssLabel = document.createElement('div');
+  cssLabel.id = 'label';
+  htmlLabel.innerHTML = 'CSS Code';
+  code.appendChild(cssLabel);
+
+  var cssCode = document.createElement('textarea');
+  cssCode.className = 'code';
+  cssCode.id = 'cssCode';
+  cssCode.innerHTML = '.test {' + '\n' + '  background-color: red;' + '\n' + '  border-radius: 10px;' + '\n' + '  width: 100px;' + '\n' + '  height: 50px;' + '\n' + '  top: 0px;' + '\n' + 'left: 0px;' + '\n' + '}';
+  code.appendChild(cssCode);
+
+  var jsLabel = document.createElement('div');
+  jsLabel.className = 'label';
+  jsLabel.innerHTML = 'Javascript Code';
+  code.appendChild(jsLabel);
+
+  var jsCode = document.createElement('textarea');
+  jsCode.className = 'code';
+  jsCode.id = 'jsCode';
+  code.appendChild(jsCode);
+
+  tryIt.appendChild(code);
+
+  // create iframe and append to the section
+  var iframe = document.createElement('iframe');
+  iframe.id = 'display';
+  tryIt.appendChild(iframe);
 }
