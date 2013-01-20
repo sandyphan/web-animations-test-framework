f4027f57b756ce3eebc3ae0d696e289070fe985b
Sandy Phan <sandyphan@google.com>
2013-01-21 10:30:21 +1100

    made sure the link to polyfill is correct and always there, commented out the solution box

--- tutorial/tryItYourself.js
+++ tutorial/tryItYourself.js
@@ -12,9 +12,9 @@ var cssEle = document.createElement('style');
 // js scripts
 var displayDefault = function() {
   frames['display'].document.documentElement.innerHTML = htmlVal;
-  includes.setAttribute('src', '../../web-animations-js/web-animation.js');
   cssEle.innerHTML = cssVal;
-  appendObjects();
+  par = frames['display'].document.getElementsByTagName('style')[0];
+  par.appendChild(cssEle);
 }
 
 // executed when button called update is clicked
@@ -26,16 +26,21 @@ function update() {
   htmlVal = document.getElementById('htmlCode').value;
   cssVal = document.getElementById('cssCode').value;
   var jsVal = document.getElementById('jsCode').value;
+  includes.setAttribute('src', '../../web-animations-js/web-animation.js');
 
   // change the body and css in value in inframe
   frames['display'].document.documentElement.innerHTML = htmlVal;
   
   var par;
-
+  
   // change the scripts in iframe
+  par = frames['display'].document.getElementsByTagName('body')[0];
+  console.log(includes);
+  par.appendChild(includes);
+  
   if (frames['display'].document.getElementsByTagName('script')[1]) {
     var oldScript = frames['display'].document.getElementsByTagName('script')[1];
-    scriptEle.innerHTML = "\n" + jsVal + "\n";
+    scriptEle.innerHTML = '\n' + jsVal + '\n';
     frames['display'].document.getElementsByTagName('body')[0].replaceChild(scriptEle, oldScript);
   } else {
     scriptEle.innerHTML = jsVal;
@@ -48,10 +53,8 @@ function update() {
 }
 
 var appendObjects = function() {
-  var par = frames['display'].document.getElementsByTagName('body')[0];
-  par.appendChild(includes);
-  par = frames['display'].document.getElementsByTagName('head')[0];
-  par.appendChild(cssEle);
+  
+  
 }
 
 // make the solution box toggleable
