a546b74436a422b760efef3d80d50dc3ad6f8a28
Steph McArthur <stephmc.tdu@gmail.com>
2013-01-25 23:40:34 +1100

    reformatting

--- extra-asserts.js
+++ extra-asserts.js
@@ -13,10 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- /*TO DO:
+ /*TODO:
  - incorperate object notation (JSON) varibles for the test so it is easier to call
  - Change the pause method for flashing so it doesn't rely on par groups. This requires the 
-    ability to either globally pause or check if a animation is currently playing 
+    ability to either globally pause or check if a animation is currently playing
+ - Make sure this is compatible with all browsers
  */
 
 var animObjects = []; //to keep track of all animations
@@ -111,7 +112,6 @@ function setupTests(timeouts){
 
 //Adds each test to a list to be processed when runTests is called
 function check(object, property, target, time, message){
-  //console.log("chheeeeee");
   if(testStack.length == 0 && refTestStack.length == 0) reparent();
   //Create new async test
   var test = async_test(message);
@@ -139,14 +139,11 @@ function check(object, property, target, time, message){
 
 function reparent(){
   //Put all the animations into a par group to get around global pause issue/bug
-  //console.log(document.animationTimeline.children);
   var childList = [];
   for (var i = 0; i < document.animationTimeline.children.length; i++) {
     childList.push(document.animationTimeline.children[i]);
   }
-  //parentAnimation = new ParGroup(document.animationTimeline.children);
   parentAnimation = new ParGroup(childList);
-  //console.log(document.animationTimeline.children);
 }
 
 //Call this after lining up the tests with check
@@ -219,17 +216,12 @@ function runAutoTest(){
     }
   }
   if(testIndex < testPacket.length){
-    //move the entire animation to the right point in time
-
     //enough to let the first frame render
     //stops bug: where at time zero if x is blue then is told to animate from red to green
     //and a check is performed at time zero for color red it checked when x was still blue
-    if(testPacket[testIndex][0].time == 0 ){
-      testPacket[testIndex][0].time += 0.02;
-    } 
-    // for(x in animObjects){
-    //   //animObjects[x]["currentTime"] = testPacket[testIndex][0].time;
-    // }
+    if(testPacket[testIndex][0].time == 0 ) testPacket[testIndex][0].time += 0.02;
+
+    //move the entire animation to the right point in time
     document.animationTimeline.children[0].currentTime = testPacket[testIndex][0].time;
     testIndex++;
     window.webkitRequestAnimationFrame(function(){runAutoTest();});
@@ -241,8 +233,7 @@ function runAutoTest(){
 
 function animTimeViewer(){
   var currTime = document.animationTimeline.children[0].iterationTime; 
-  //console.log(document.animationTimeline.children[0]);
-  //currTime = currTime.toFixed(2);
+  currTime = currTime.toFixed(2);
   var object = document.getElementById("animViewerText");
   var comp = object.currentStyle || getComputedStyle(object, null);
   object.innerHTML = "Current animation time " + currTime;
@@ -253,16 +244,13 @@ function animTimeViewer(){
 //to lag, causing them to fail. This method should test as often as auto (no more or no less)
 function refTestRunner(index){
   if(index == null) index = 0;
-  //as soon as the current frame time is over a ref test then pop it off and run the test
+  //as soon as the current frame time is over a ref test time then pop it off and run the test
   var doNextTest = false;
   if(refTestStack.length > 0) doNextTest = true;
   while(doNextTest && index < refTestStack.length){
     var currTest = refTestStack[index];
     if(currTest.time <= document.animationTimeline.children[0].iterationTime){
-      //console.log(currTest);
       doNextTest = true;
-      //console.log(currTest.object);
-      //console.log(currTest.target);
       currTest.test.step(function (){
         assert_properties(currTest.object, currTest.property, currTest.target, currTest.message);
       });
@@ -281,7 +269,7 @@ function restart(){
   window.location.href = url[0] + "?" + state;
 }
 
-// create divs at appropriate locations and flash the divs for manual testing
+// create elements at appropriate locations and flash the elements for manual testing
 function flashing(test) {
   parentAnimation.pause();
   var type = test.object.nodeName;
@@ -292,8 +280,6 @@ function flashing(test) {
     var flash = document.createElementNS("http://www.w3.org/2000/svg", type);
   }
    
-  //console.log(flash);
-
   if(type == "DIV") document.getElementById("test").appendChild(flash);
   else document.getElementsByTagName("svg")[0].appendChild(flash);
 
@@ -408,7 +394,8 @@ function assert_properties(object, props, targets, message, epsilons){
       }
     } else {
       for(var i = 0; i < props.length; i++){
-        if(props[i].indexOf("olor") != -1){ //for anything with the word color in it do the color assert (C is not there because it could be a c or C)
+		//for anything with the word color in it do the color assert (C is not there because it could be a c or C)
+        if(props[i].indexOf("olor") != -1){ 
           assert_color(object, targets[i], message);
         } else if(props[i] == "style"){
           assert_webkit_style(object, targets[x], message);
