076b7b6314c0a508996551286241139ea3fb4e87
Sarah Heimlich <sarahm.heimlich@gmail.com>
2013-02-04 14:24:19 +1100

    First commit that creates the try it yourself display

--- tutorial/tryItYourself.js
+++ tutorial/tryItYourself.js
@@ -101,6 +101,72 @@ Iframe.prototype.setTime = function(newTime) {
   this.time = newTime;
 }
 
+function heading() {
+  var heading = document.createElement("div");
+  heading.setAttribute("class", "heading");
+  heading.setAttribute('id', 'heading')
+  heading.innerHTML = "TRY IT YOURSELF";
+  document.getElementById("tryIt").appendChild(heading);
+
+  var button = document.createElement('button');
+  button.setAttribute('onclick', 'update()');
+  button.setAttribute('id', 'update');
+  button.innerHTML = "Update";
+  document.getElementById('heading').appendChild(button);
+
+   var code = document.createElement('div');
+   code.setAttribute('class', 'code');
+   code.setAttribute('id', 'allCode');
+   document.getElementById("tryIt").appendChild(code);
+
+   var html = document.createElement('div');
+   html.setAttribute('class', 'label');
+   html.setAttribute('id', 'htmlLabel')
+   html.innerHTML = "HTML Code";
+   document.getElementById('allCode').appendChild(html);
+
+   var htmlCode = document.createElement('textarea');
+   htmlCode.setAttribute('id', 'htmlCode');
+   htmlCode.setAttribute('class', 'code');
+   htmlCode.innerHTML = "<div id='a' class='test'></div>";
+   document.getElementById('htmlLabel').appendChild(htmlCode);
+
+   var css = document.createElement('div');
+   css.setAttribute('class', 'label');
+   css.setAttribute('id', 'cssLabel')
+   css.innerHTML = "CSS Style";
+   document.getElementById('allCode').appendChild(css);
+
+   var cssCode = document.createElement('textarea');
+   cssCode.setAttribute('id', 'cssCode');
+   cssCode.setAttribute('class', 'code');
+   cssCode.innerHTML = ".test {"
+     +"\n" + "background-color: red;"
+     +"\n" + "border-radius: 10px;"
+     +"\n" + "width: 100px;"
+     +"\n" + "height: 50px;"
+     +"\n" + "top: 50px;"
+     +"\n" + "left: 0px;"
+     +"\n" + "position: absolute;"
+     +"\n" + "}";
+   document.getElementById('cssLabel').appendChild(cssCode);
+
+   var js = document.createElement('div');
+   js.setAttribute('class', 'label');
+   js.setAttribute('id', 'jsLabel')
+   js.innerHTML = "Javascript";
+   document.getElementById('allCode').appendChild(js);
+
+   var jsCode = document.createElement('textarea');
+   jsCode.setAttribute('id', 'jsCode');
+   jsCode.setAttribute('class', 'code');
+   document.getElementById('jsLabel').appendChild(jsCode);
+
+   var display = document.createElement('div');
+   display.setAttribute('class', 'display');
+   document.getElementById('tryIt').appendChild(display);
+}
+
 // innerDoc the solution box toggleable*/
 var toggleSolution = function() {
   var ele = document.getElementById('toggleText');
